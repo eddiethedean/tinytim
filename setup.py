23a8cf7e0c30ec534c0322db27f3ce39edf5df33
Odos Matthews <odosmatthews@Odoss-MacBook-Pro.local>
2022-08-21 09:34:59 -0400

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="tinytim",
-    version="0.2",
+    version="0.3",
     description="Pure Python data table functions.",
     long_description=README,
     long_description_content_type="text/markdown",
