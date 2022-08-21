3f1857b97c95643b9a6c3ae9b7c2b5832625d8ae
Odos Matthews <odosmatthews@Odoss-MacBook-Pro.local>
2022-08-21 11:57:20 -0400

    changed init imports

--- tinytim/__init__.py
+++ tinytim/__init__.py
@@ -1,9 +1,9 @@
-import tinytim.columns
-import tinytim.copy
-import tinytim.edit
-import tinytim.features
-import tinytim.filter
-import tinytim.group
-import tinytim.rows
-import tinytim.utils
-import tinytim.validate
+from tinytim.columns import itercolumns
+from tinytim.copy import copy_table
+from tinytim.edit import drop_row
+from tinytim.features import head, tail, size, shape
+from tinytim.filter import sample
+from tinytim.group import groupby
+from tinytim.rows import iterrows
+
+
