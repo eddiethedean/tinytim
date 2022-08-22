562b7f0c14052346db664dce574ea41f534ab7cb
Odos Matthews <odosmatthews@odoss-mac-mini.lan>
2022-08-22 11:05:38 -0400

    edit setup

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
         "Programming Language :: Python :: 3.6",
     ],
     packages=find_packages(where='src'),
+    package_dir={'': 'src'},
     include_package_data=True,
     python_requires='>=3.6',
     install_requires=[]
