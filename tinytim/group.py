6754e4cf72b6cdfb9e912d2d2605e13702f397c3
Odos Matthews <odosmatthews@odoss-mac-mini.lan>
2022-09-09 11:58:59 -0400

    changed groupby key to tuple of values

--- tinytim/group.py
+++ tinytim/group.py
@@ -1,5 +1,4 @@
-from collections import namedtuple
-from typing import Any, Callable, Dict, List, Mapping, Optional, Sequence, Tuple, Union
+from typing import Any, Callable, Dict, List, Mapping, Sequence, Tuple, Union
 
 from tinytim.filter import column_filter, filter_data
 from tinytim.utils import row_dicts_to_data, row_value_tuples, uniques
@@ -32,46 +31,26 @@ def groupby(data: Mapping, by: Union[str, Sequence[str]]) -> List[tuple]:
         return groupbymulti(data, by)
 
 
-def _keys(key, by) -> dict:
-    keys = {}
-    if isinstance(by, str):
-        keys[by] = key
-    else:
-        for col, k in zip(by, key):
-            keys[col] = k
-    return keys
-
-
 def aggregate_groups(
     groups: Sequence[Tuple[Any, DataMapping]],
-    by: Sequence[str],
-    func: Callable[[DataMapping], RowMapping],
-    tuplename: Optional[str] = 'GroupbyKey'
+    func: Callable[[DataMapping], RowMapping]
 ) -> Tuple[List, DataDict]:
     labels = []
     rows = []
     for key, data in groups:
         row = func(data)
         if len(row):
-            GroupbyKey = namedtuple(field_names=by, typename=tuplename)
-            keys = _keys(key, by)
-            labels.append(GroupbyKey(*keys.values()))
+            labels.append(key)
             rows.append(row)
     return labels, row_dicts_to_data(rows)
 
 
-def sum_groups(
-    groups: List[tuple],
-    by: Sequence[str]
-) -> Tuple[List, DataDict]:
-    return aggregate_groups(groups, by, sum_data, 'Sums')
+def sum_groups(groups: List[tuple]) -> Tuple[List, DataDict]:
+    return aggregate_groups(groups, sum_data)
 
 
-def count_groups(
-    groups: List[tuple],
-    by: Sequence[str]
-) -> Tuple[List, DataDict]:
-    return aggregate_groups(groups, by, count_data, 'Counts')
+def count_groups(groups: List[tuple]) -> Tuple[List, DataDict]:
+    return aggregate_groups(groups, count_data)
 
 
 def aggregate_data(data: Mapping, func: Callable) -> RowDict:
